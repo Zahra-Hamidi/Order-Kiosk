0c933c69cd5b7a550079486cca57c68cef966c23
Pc.unique <zahraeng369@gmail.com>
2021-06-29 19:53:50 +0430

    design HomeScreen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { CssBaseline,ThemeProvider ,Container,Paper, createMuiTheme} from "@material-ui/core";
+import HomeScreen from './screens/HomeScreen'
 
+
+const theme = createMuiTheme({
+  typography:{
+    h1:{
+      fontWeight:'bold'
+    },
+    h2:{
+      fontSize:'2rem',
+      color:'black'
+    },
+    h3:{
+      fontSize:'1.8rem',
+      fontWeight:'bold',
+      color:'white'
+    }
+  },
+  palette:{
+    primary:{main:'#ff1744'},
+    secondary:{
+      main:'#118e16',
+      contrastText:'#ffffff'
+    }
+  }
+})
 
 function App() {
   return (
-    <div>
-     
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Container maxWidth="sm">
+        <Paper>
+          <HomeScreen></HomeScreen>
+        </Paper>
+      </Container>
+    </ThemeProvider>
   );
 }
 
